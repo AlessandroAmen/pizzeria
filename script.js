52d06398bad4b5cd9c8356a1afae96f0c0cab163
AlessandroAmen <xela9827@gmail.com>
2024-12-22 19:27:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const btnCerca = document.getElementById('btnCerca');
+const formRicerca = document.getElementById('formRicerca');
+
+btnCerca.addEventListener('click', () => {
+  btnCerca.style.display = 'none';
+  formRicerca.style.display = 'block';
+}); 
